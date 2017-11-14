9cc15796f880797ee5f806940ed6866066c818d8
Edu <Edu@eduarts-mbp.edu.ds.fhnw.ch>
2017-11-14 10:21:24 +0100

    ffsf

--- finaldummygui/lobby/Lobby_Main.java
+++ finaldummygui/lobby/Lobby_Main.java
@@ -14,9 +14,9 @@ public class Lobby_Main extends Application {
 		Lobby_Controller c = new Lobby_Controller();	
 	}
 	
-	public static void main (String[] args){
-		launch(args);
-	}
+	//public static void main (String[] args){
+	//	launch(args);
+	//}
 	
 
 
