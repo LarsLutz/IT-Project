241f7056e2e08d2235dd3e3d08475958528389dc
Jan Müller <Jan@Jans-MacBook-Pro.local>
2017-11-15 09:34:29 +0100

    wf

--- finaldummygui/login/Login_View.java
+++ finaldummygui/login/Login_View.java
@@ -10,28 +10,28 @@ import javafx.stage.Stage;
 public class Login_View {
 	
 
-	final private Stage stage;
+	//final private Stage stage;
 	
-	public Login_View(Stage stage){
+	//public Login_View(Stage stage){
 		
-		this.stage = stage;
+		//this.stage = stage;
 		
-		stage.setTitle("Login");
+		//stage.setTitle("Login");
 		
 
-		try {
-			AnchorPane root =  (AnchorPane) FXMLLoader.load(getClass().getResource("LoginGUI.fxml"));
-			Scene scene = new Scene(root,700,500);
-			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			stage.setScene(scene);
-			stage.show();
-		} catch(Exception e) {
-			e.printStackTrace();
-		}
+		//try {
+			//AnchorPane root =  (AnchorPane) FXMLLoader.load(getClass().getResource("LoginGUI.fxml"));
+			//Scene scene = new Scene(root,700,500);
+			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			//stage.setScene(scene);
+			//stage.show();
+		//} catch(Exception e) {
+		//	e.printStackTrace();
+		//}
 		
 		
 	
-	}
+	//}
 	
 
 	
