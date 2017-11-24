d38b911fa9ea2d74b9a35f5ff2a699710bec3cf7
LarsLutz <32195663+LarsLutz@users.noreply.github.com>
2017-11-24 15:31:10 +0100

    Evt. Wirds rot

--- Lars/mysql/MYSQL.java
+++ Lars/mysql/MYSQL.java
@@ -1,15 +1,11 @@
 package mysql;
 
+
+import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
-
-import javax.naming.Context;
-import javax.naming.InitialContext;
-import javax.sql.DataSource;
-
-import com.mysql.jdbc.Connection;
 
 
 public class MYSQL {
