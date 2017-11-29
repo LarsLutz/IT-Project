2355ecb9c5fe3c45a4a669b8cd117ba613b3ff29
EduBunjaku <eduartbunjaku@gmx.ch>
2017-11-29 11:47:23 +0100

    comparable
    update

--- Dominion/Logik/Siegesbedingung.java
+++ Dominion/Logik/Siegesbedingung.java
@@ -1,5 +1,11 @@
 package Logik;
 
-public class Siegesbedingung {
+public class Siegesbedingung implements Comparable{
+
+	@Override
+	public int compareTo(Object o) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 }
