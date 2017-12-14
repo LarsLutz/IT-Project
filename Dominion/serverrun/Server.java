23d1f0653f53cf313dffc684c79074618b09a4f3
LarsLutz <32195663+LarsLutz@users.noreply.github.com>
2017-12-14 15:01:55 +0100

    Server

--- Dominion/serverrun/Server.java
+++ Dominion/serverrun/Server.java
@@ -5,44 +5,75 @@ import java.net.ServerSocket;
 import java.net.URL;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
 
-public class Server{
+public class Server implements Runnable {
+	
+
 
     static boolean running = true;
+    private String close="";
     
-    
-    public static void serverStart(){
-        try {
-        	System.out.println("Sever startet");
-            ServerSocket serverSocket = new ServerSocket(50663); // Generiert Serversocket
-            Player player1 = new Player(serverSocket.accept(), 1); //Instansiert die Klasse Player
-            Player player2 = new Player(serverSocket.accept(), 2);
-            while (player1.isRunning()&player2.isRunning()){ //Soblad beide Verbunden sind
-                //Daten zuerst verarbeiten bevor weitergeleitet wird
-
-                if(player1.isDataInAvailable()){
-                    String data = player1.getDataIn();
-                    //Verarbeiter.verarbeite(player1.getdatain(),player1,player2)
-                    System.out.println("1 - 2");
-                    player2.setDataOut(data);
-                    System.out.println("echo");
-                    player1.setDataOut(data);
-                }
-                if(player2.isDataInAvailable()){
-                    String data = player2.getDataIn();
-                    System.out.println("2 - 1");
-                    player1.setDataOut(data);
-                    System.out.println("echo");
-                    player2.setDataOut(data);
-                }
-            }
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
+    public String getClose() {
+    	return close;
     }
+
+
+
+
+    public void setClose(String close) {
+    	this.close = close;
+    }
+     
+    
+    
+    
+    
+ @Override   
+public void run() {
 	
+	try {
+    	System.out.println("Sever startet");
+    	
+        ServerSocket serverSocket = new ServerSocket(50663); // Generiert Serversocket
+        Player player1 = new Player(serverSocket.accept(), 1); //Instansiert die Klasse Player
+        Player player2 = new Player(serverSocket.accept(), 2);
+        while (player1.isRunning()&player2.isRunning()){ //Soblad beide Verbunden sind
+            //Daten zuerst verarbeiten bevor weitergeleitet wird
+
+            if(player1.isDataInAvailable()){
+                String data = player1.getDataIn();
+                //Verarbeiter.verarbeite(player1.getdatain(),player1,player2)
+                System.out.println("1 - 2");
+                player2.setDataOut(data);
+                System.out.println("echo");
+                player1.setDataOut(data);
+            }
+            if(player2.isDataInAvailable()){
+                String data = player2.getDataIn();
+                System.out.println("2 - 1");
+                player1.setDataOut(data);
+                System.out.println("echo");
+                player2.setDataOut(data);
+            }
+        }
+    } catch (IOException ex) {
+        ex.printStackTrace();
+    }
 }
+
+
+
+
+
+
+		
+	}
+    
+    
+	
+
--- Dominion/serverrun/Server_Controller.java
+++ Dominion/serverrun/Server_Controller.java
@@ -1,10 +1,24 @@
 package serverrun;
 
+import java.net.InetAddress;
+import java.net.UnknownHostException;
+
+import javafx.application.Platform;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 
 public class Server_Controller {
+	
+	
+	private Thread thread;
+	private Server server;
+	private String ip;
+	private Player player;
+	
+	public  Server_Controller() {
+		
+	}
 	
 	@FXML
 	Label l1, l2, lStatus, lAdresse;
@@ -13,8 +27,33 @@ public class Server_Controller {
 	Button bSchliessen;
 	
 	public void initialize(){
-		System.out.println("Hallo Welt");
-		Server.serverStart();
+		
+		InetAddress address;
+		try {
+			address = InetAddress.getLocalHost();
+			ip = address.toString();
+		} catch (UnknownHostException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
+		
+		
+		
+		server= new Server();
+		this.thread=new Thread(server);
+		this.thread.start();
+		lStatus.setText("running");
+		lAdresse.setText(ip);
+		
+	}
+	
+	@FXML
+	public void schliessenKlick(){
+		lStatus.setText("stopped");
+		server.setClose("$end");
+		Platform.exit();
+		
 	}
 	
 }
